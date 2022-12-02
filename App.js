ca86332a08e3ae9d9a9daa681a7b8db43ad83656
lukeverso <cristhovamlucas@gmail.com>
2022-12-02 08:06:33 -0300

    Terceiro commit

--- App.js
+++ App.js
@@ -1,20 +1,29 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+
 import Task from './src/pages/Task';
 import NewTask from './src/pages/NewTask';
 import Details from './src/pages/Details';
 import Login from './src/pages/Login';
+import Register from './src/pages/Register';
 
 const Stack = createStackNavigator();
 
 export default function App() {
      return (
           <NavigationContainer>
-               <Stack.Navigator initialRouteName='Task'>
+               <Stack.Navigator initialRouteName='Login'>
                     <Stack.Screen
                          name='Login'
                          component={Login}
+                         options={{
+                              headerShown: false
+                         }}
+                    />
+                    <Stack.Screen
+                         name='Register'
+                         component={Register}
                          options={{
                               headerShown: false
                          }}
